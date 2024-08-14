6ff8ac6f6905e7806e56986220f179b8bd5bc734
Jordan Taylor <66581302+J22T@users.noreply.github.com>
2024-08-14 12:09:42 -0700

    Adding Operators

--- projects/calculator/calculator.py
+++ projects/calculator/calculator.py
@@ -1,2 +1,18 @@
-number_1 = input('Enter your first number: ')
-number_2 = input('Enter your second number: ')
+number_1 = int( input('Enter your first number: '))
+number_2 = int( input('Enter your second number: '))
+
+# Addition
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+
+# Subtraction
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+# Multiplication
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+# Division
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
