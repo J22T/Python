bd487801dd9ebd9718721bd877eceaca34bdb3a5
Jordan Taylor <66581302+J22T@users.noreply.github.com>
2024-09-05 15:36:27 -0700

    cleaning

--- projects/strings.py
+++ projects/strings.py
@@ -25,3 +25,38 @@
 
 # course = 'Python for Beginners'
 # print('Python' in course)
+
+
+
+# course = "Python for Beginners"
+# print(course)
+
+# course = 'Python for "Beginners"'
+# print(course)
+
+# course = '''
+# This is an email triple quotes test
+# '''
+# print(course)
+
+# course = 'Python for Beginners'
+# print(course[0])
+
+# course = 'Python for Beginners'
+# print(course[0:3])
+
+# course = 'Python for Beginners'
+# print(course[0:])
+
+# course = 'Python for Beginners'
+# print(course[:5])
+
+# course = 'Python for Beginners'
+# print(course[:])
+
+# course = 'Python for Beginners'
+# another = course[:]
+# print(another)
+
+# name = 'Jordan'
+# print(name[1:-1])
--- projects/type-conversion.py
+++ projects/type-conversion.py
@@ -29,3 +29,7 @@
 # second = float(input("Second: "))
 # sum = first + second
 # print("Sum: " + str(sum))
+
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
